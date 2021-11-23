7920b34ac880fd689f1ac2474d3f9d6a5b0b45fe
Sukhpreet kaur <77190311+sukh-01@users.noreply.github.com>
2021-11-23 19:40:13 +0530

    Update functions.c

--- Miniproject_C/3_Implementation/src/functions.c
+++ Miniproject_C/3_Implementation/src/functions.c
@@ -1,1 +1,60 @@
+#include "compute.h"
+		
+void show_record()
+    {system("cls");
+	char name[20];
+	float scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&scr);
+	printf("\n\n\t\t*************************************************************");
+	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
+	printf("\n\n\t\t*************************************************************");
+	fclose(f);
+	getch();}
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);}
+
+void help()
+	{system("cls");
+    printf("\n\n                              HELP");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n ......................... C program Quiz Game...........");
+    printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
+    printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
+    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded $100,000.");
+    printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+    printf("\n >> No negative marking for wrong answers");
+
+	printf("\n\n\t*********************BEST OF LUCK*********************************");
+	printf("\n\n\t*****C PROGRAM QUIZ GAME is developed by CODE WITH C TEAM********");}
+
+void edit_score(float score, char plnm[20])
+	{system("cls");
+	float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	  { sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%.2f",plnm,sc);
+	    fclose(f);}}
 
