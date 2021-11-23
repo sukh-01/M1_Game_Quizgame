9e46289b0ab239387902b00a9ebe0b786a3a850f
Sukhpreet kaur <77190311+sukh-01@users.noreply.github.com>
2021-11-23 19:42:09 +0530

    Update quizhead.h

--- Miniproject_C/3_Implementation/inc/quizhead.h
+++ Miniproject_C/3_Implementation/inc/quizhead.h
@@ -1,1 +1,9 @@
-
+#include "stdio.h"
+#include "conio.h"
+#include "ctype.h"
+#include "stdlib.h"
+#include "string.h"
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
